48a53179ef245ebc28e2ffc86b6de5be5850313e
Manmeet Singh <singhmanmeet2222@gmail.com>
2017-08-21 23:42:53 +0530

    Q6

---
+++ Questions/Q6.py
@@ -0,0 +1,9 @@
+x = int(input("Enter first number"))
+y = int(input("Enter second number"))
+
+if (x>y):
+    print(x," is greater than ",y)
+elif(x<y):
+    print(y, " is greater than ", x)
+else :
+    print("Both are equal")
