af1160904fcd5ac75100b7077a723850588c9f36
Manmeet Singh <singhmanmeet2222@gmail.com>
2017-08-30 23:34:24 +0530

    Q8

---
+++ Questions/Q7.py
@@ -0,0 +1,13 @@
+x = float(input("Enter your score"))
+if (x>=.9):
+    print("A")
+elif (x>=.8):
+    print("B")
+elif (x>=.7):
+    print("C")
+elif (x>=.6):
+    print("D")
+elif (x>=.5):
+    print("E")
+else:
+    print("F")
